128f3d4c0db0b678d0745d0246572662a5bdcb27
Alex <anadreau42@gmail.com>
2023-01-11 22:27:36 -0700

    Added test for encrypt and decode

--- test/cipher_test.dart
+++ test/cipher_test.dart
@@ -1,8 +1,12 @@
+import 'package:cipher/decode.dart';
 import 'package:cipher/encrypt.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('CipherText', () {
+  test('CipherText encrpyt', () {
     expect(encrypt('a b c', 1), 'b c d');
+  });
+  test('CipherText decode', () {
+    expect(decode('b c d', 1), 'a b c');
   });
 }
