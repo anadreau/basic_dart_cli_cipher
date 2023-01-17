e54dfb8ba37a9bbd46f6a00171d217679e84b93d
Alex <anadreau42@gmail.com>
2023-01-16 21:23:57 -0700

    Added visual seperator to cmd line

--- bin/decrypt.dart
+++ bin/decrypt.dart
@@ -3,10 +3,12 @@ import 'dart:io';
 import 'package:cipher/decrypt.dart';
 
 void main(List<String> arguments) {
+  stdout.write('************\n');
   stdout.write('Input key: ');
   var key = int.parse(stdin.readLineSync().toString());
   stdout.write('Decode message:');
   var input = stdin.readLineSync();
   String decodedMessage = decrypt(input!, key);
-  stdout.write('Message: $decodedMessage ');
+  stdout.write('Message: $decodedMessage\n');
+  stdout.write('************\n');
 }
--- bin/encrypt.dart
+++ bin/encrypt.dart
@@ -3,10 +3,12 @@ import 'dart:io';
 import 'package:cipher/encrypt.dart';
 
 void main(List<String> arguments) {
+  stdout.write('************\n');
   stdout.write('Input key: ');
   var key = int.parse(stdin.readLineSync().toString());
   stdout.write('Encrypt message:');
   var input = stdin.readLineSync();
   String cipherText = encrypt(input!, key);
-  stdout.write('ciphertext: $cipherText ');
+  stdout.write('ciphertext: $cipherText\n');
+  stdout.write('************\n');
 }
